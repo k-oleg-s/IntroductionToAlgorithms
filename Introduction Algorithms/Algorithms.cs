ec3b44e105249192fa07bd5cbf1f010f293aa750
OlegKS <k_olegs@mail.ru>
2022-08-24 00:31:25 +0300

    Lesson1 done

---
+++ Introduction Algorithms/Algorithms.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction_Algorithms;
+
+static public class Algorithms
+{
+    static public string SimpleNumber(int n)
+    {
+        int d = 0;
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0) d++;
+        }
+        if (d == 0) { Console.WriteLine($" {n} простое"); return "y"; }
+        else { Console.WriteLine($" {n} НЕ простое"); return "n"; }
+    }
+
+    static int StrangeSum(int[] inputArray)   // O(n^3)
+    {
+        int sum = 0;
+        for (int i = 0; i < inputArray.Length; i++) // n
+        {
+            for (int j = 0; j < inputArray.Length; j++) // n*n
+            {
+                for (int k = 0; k < inputArray.Length; k++)          // n*n*n              
+                {
+                    int y = 0;
+                    if (j != 0)
+                    {
+                        y = k / j;
+                    }
+                    sum += inputArray[i] + i + k + j + y;
+                }
+            }
+        }
+        return sum;
+    }
+
+    static public int F_cycle(int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int prev1 = 1;
+        int prev2 = 1;
+        int current = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            if (i==2)
+            {
+                prev2 = 0; prev1 = 1;
+            }
+            current = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return current;
+    }
+
+    static public int F_recurse(int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return F_recurse(n-2)+F_recurse(n-1);
+    }
+}
--- Introduction Algorithms/Program.cs
+++ Introduction Algorithms/Program.cs
@@ -1,2 +1,49 @@
-﻿ 
+﻿using Introduction_Algorithms;
+
 Console.WriteLine("Start");
+
+
+TestCase mainTester = new TestCase();
+List<TestCase> tests = new List<TestCase>();
+
+
+// Простое число
+
+tests.Add(new TestCase() { X = 17, ExpectedS = "y" });
+tests.Add(new TestCase() { X = 29, ExpectedS = "y" });
+tests.Add(new TestCase() { X = 43, ExpectedS = "y" });
+tests.Add(new TestCase() { X = 14, ExpectedS = "n" });  //не простое
+tests.Add(new TestCase() { X = 25, ExpectedS = "n" });  //не простое
+
+Console.WriteLine("     Простое число");
+foreach (TestCase testCase in tests)
+{
+    mainTester.TestSimpleNumber(testCase);
+}
+tests.Clear();
+
+
+
+//числа Фибоначчи
+// 0	1	2	3	4	5	6	7   9   10
+// 0	1	1	2	3	5	8	13  34  55
+//tests.Add(new TestCase() { X = 0, ExpectedI = 0 });
+tests.Add(new TestCase() { X = 2, ExpectedI = 1 });
+tests.Add(new TestCase() { X = 4, ExpectedI = 3 });
+tests.Add(new TestCase() { X = 7, ExpectedI = 13 });
+tests.Add(new TestCase() { X = 9, ExpectedI = 33 });    // must be 34
+tests.Add(new TestCase() { X = 10, ExpectedI = 54 });   // must be 55
+
+Console.WriteLine("\n \n   числа Фибоначчи");
+foreach (TestCase testCase in tests)
+{
+    Console.WriteLine($" for X={testCase.X}  Expected result:{testCase.ExpectedI}");
+    mainTester.TestFiboRecursive(testCase);
+    mainTester.TestFiboCycleFor(testCase);
+    Console.WriteLine(" ");
+}
+
+tests.Clear();
+
+
+Console.ReadLine();
