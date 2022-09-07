fe891d6d8231e38c5f2d28c498329f0687876ab9
OlegKS <k_olegs@mail.ru>
2022-09-07 22:59:28 +0300

    скопировал вспомогательный код к заданию 2.

--- Introduction Algorithms/Program.cs
+++ Introduction Algorithms/Program.cs
@@ -1,2 +1,13 @@
-﻿ 
+﻿
+using BenchmarkDotNet.Running;
+using Introduction_Algorithms;
+using System.Text;
+
 Console.WriteLine("Start");
+
+BenchmarkRunner.Run<TestUnit>();
+
+//var t = new TestUnit();
+//t.FillStringContainers();
+//t.IsExistsInMassive("asd");
+//t.IsExistsInHashSet("asd");
---
+++ Introduction Algorithms/TreeNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction_Algorithms;
+
+public class TreeNode
+{
+    public int Value { get; set; }
+    public TreeNode LeftChild { get; set; }
+    public TreeNode RightChild { get; set; }
+    public override bool Equals(object obj)
+    {
+        var node = obj as TreeNode;
+        if (node == null)
+            return false;
+        return node.Value == Value;
+    }
+}
